260f65e6ebb8277f6c18cb36a918d582953531ca
Gregory Tomilin <gregory.tomilin@gmail.com>
2020-08-12 17:52:22 +0300

    Слайдер

--- js/app.js
+++ js/app.js
@@ -5,14 +5,70 @@ let $menu = document.querySelector('.menu');
 $hamburgerMenuBtn.addEventListener('click', (e)=>{
     e.preventDefault();
     $menu.classList.add('visible')
-    // $menu.style.visibility = 'visible';
-    // $menu.style.opacity = '1';
 
 })
 
 $closeMenuBtn.addEventListener('click', (e)=>{
     e.preventDefault();
     $menu.classList.remove('visible')
-    // $menu.style.visibility = 'hidden';
-    // $menu.style.opacity = '0';
+
 })
+
+
+
+
+
+
+// ************************  Слайдер товаров
+
+const arrowsRight = document.querySelector('.slider__right');
+const arrowsLeft = document.querySelector('.slider__left');
+
+
+const slideWidth = document.querySelector('.slider__item');
+const rootSlider = document.getElementById('sliderRoot');
+const sliderList = document.querySelector('.slider__list');
+
+console.log(rootSlider.scrollWidth);
+console.log(sliderList.scrollWidth);
+console.log(sliderList.clientWidth);
+
+
+let sliderPosition = 0;
+
+
+
+// обработчики нажатия правых стрелок
+arrowsRight.addEventListener('click', () => moveRight());
+// обработчики нажатия левых стрелок
+arrowsLeft.addEventListener('click', () => moveLeft());
+
+// Свайпы
+// sliderList.addEventListener('swiped-right', ()=> moveLeft());
+// sliderList.addEventListener('swiped-left', ()=> moveRight());
+
+
+function moveRight() {
+    sliderPosition+= slideWidth.clientWidth;
+    console.log(sliderPosition);
+
+
+    if (sliderPosition === sliderList.scrollWidth){
+        sliderPosition = 0;
+    }
+    rootSlider.style.transform = `translate(-${sliderPosition}px)`;
+}
+
+function moveLeft() {
+    console.log(sliderPosition);
+
+    if (sliderPosition === 0){
+        sliderPosition = sliderList.scrollWidth - slideWidth.clientWidth;
+    }else{
+        sliderPosition -= slideWidth.clientWidth;
+    }
+    rootSlider.style.transform = `translate(-${sliderPosition}px)`;
+}
+
+
+// ************************  END Слайдер товаров
